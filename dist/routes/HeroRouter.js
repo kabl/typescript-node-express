1f157841315fdc5fd75033c8f80625be2ab9aa89
kabl <blaser_k@gmx.ch>
2017-05-08 22:02:42 +0200

    fix debug vs test

--- dist/routes/HeroRouter.js
+++ dist/routes/HeroRouter.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const debug = require("debug");
 const express_1 = require("express");
-const Heroes = require('../data');
 class HeroRouter {
     constructor() {
         this.router = express_1.Router();
@@ -14,12 +13,12 @@ class HeroRouter {
         this.router.delete('/:id', this.deleteOne);
     }
     getAll(req, res, next) {
-        res.send(Heroes);
+        res.send(data);
     }
     getOne(req, res, next) {
         let query = parseInt(req.params.id);
         debug('get by id: ' + query);
-        let hero = Heroes.find(hero => hero.id === query);
+        let hero = data.find(hero => hero.id === query);
         if (hero) {
             res.status(200)
                 .send({
@@ -38,7 +37,7 @@ class HeroRouter {
     }
     deleteOne(req, res, next) {
         let query = parseInt(req.params.id);
-        let hero = Heroes.find(hero => hero.id === query);
+        let hero = data.find(hero => hero.id === query);
         if (hero) {
             // Heroes.delete(hero.id);
             res.status(200)
@@ -60,4 +59,130 @@ exports.HeroRouter = HeroRouter;
 const heroRoutes = new HeroRouter();
 //heroRoutes.init();
 exports.default = heroRoutes.router;
+var data = [
+    {
+        "id": 1,
+        "name": "Luke Cage",
+        "aliases": ["Carl Lucas", "Power Man", "Mr. Bulletproof", "Hero for Hire"],
+        "occupation": "bartender",
+        "gender": "male",
+        "height": {
+            "ft": 6,
+            "in": 3
+        },
+        "hair": "bald",
+        "eyes": "brown",
+        "powers": [
+            "strength",
+            "durability",
+            "healing"
+        ]
+    },
+    {
+        "id": 2,
+        "name": "Spider-Man",
+        "aliases": ["Dr. Peter Benjamin Parker", "Spidey", "Web-Sligner", "Spider-X-Man"],
+        "occupation": "scientist",
+        "gender": "male",
+        "height": {
+            "ft": 5,
+            "in": 10
+        },
+        "hair": "brown",
+        "eyes": "hazel",
+        "powers": [
+            "wall-crawling",
+            "strength",
+            "speed",
+            "stamina",
+            "durability",
+            "agility",
+            "healing",
+            "reflexes",
+            "Spider-Sense",
+            "genius"
+        ]
+    },
+    {
+        "id": 3,
+        "name": "Captain America",
+        "aliases": [
+            "Winghead",
+            "Shield-Slinger",
+            "the Captain",
+            "Cap",
+            "Yeoman America",
+            "Sentinel of Liberty",
+            "The Living Legend"
+        ],
+        "occupation": "special agent",
+        "gender": "male",
+        "height": {
+            "ft": 6,
+            "in": 2
+        },
+        "hair": "blonde",
+        "eyes": "blue",
+        "powers": [
+            "strength",
+            "speed",
+            "durability",
+            "agility",
+            "reflexes",
+            "stamina",
+            "healing",
+            "longevity"
+        ]
+    },
+    {
+        "id": 4,
+        "name": "Iron Man",
+        "aliases": [
+            "Tony Stark",
+            "Golden Gladiator",
+            "Spare Parts Man",
+            "Space-Knight"
+        ],
+        "occupation": "inventor",
+        "gender": "male",
+        "height": {
+            "ft": 6,
+            "in": 1
+        },
+        "hair": "black",
+        "eyes": "blue",
+        "powers": []
+    },
+    {
+        "id": 5,
+        "name": "Wolverine",
+        "aliases": [
+            "Logan",
+            "Weapon X",
+            "Death",
+            "Agent Ten",
+            "Fist of Legend"
+        ],
+        "occupation": "special agent",
+        "gender": "male",
+        "height": {
+            "ft": 5,
+            "in": 3
+        },
+        "hair": "black",
+        "eyes": "blue",
+        "powers": [
+            "healing",
+            "acute senses",
+            "strength",
+            "speed",
+            "durability",
+            "agility",
+            "stamina",
+            "weather adaptation",
+            "animal empathy",
+            "bone claws"
+        ]
+    }
+];
 //# sourceMappingURL=HeroRouter.js.map
--- src/routes/HeroRouter.ts
+++ src/routes/HeroRouter.ts
@@ -1,6 +1,5 @@
 import * as debug from 'debug';
 import { Router, Request, Response, NextFunction } from 'express';
-const Heroes = require('../data');
 
 export class HeroRouter {
     router: Router
@@ -17,14 +16,14 @@ export class HeroRouter {
     }
 
     public getAll(req: Request, res: Response, next: NextFunction) {
-        res.send(Heroes);
+        res.send(data);
     }
 
     public getOne(req: Request, res: Response, next: NextFunction) {
         
         let query = parseInt(req.params.id);
         debug('get by id: ' + query);
-        let hero = Heroes.find(hero => hero.id === query);
+        let hero = data.find(hero => hero.id === query);
         if (hero) {
             res.status(200)
                 .send({
@@ -44,7 +43,7 @@ export class HeroRouter {
 
     public deleteOne(req: Request, res: Response, next: NextFunction) {
         let query = parseInt(req.params.id);
-        let hero = Heroes.find(hero => hero.id === query);
+        let hero = data.find(hero => hero.id === query);
         if (hero) {
             // Heroes.delete(hero.id);
             res.status(200)
@@ -67,3 +66,131 @@ const heroRoutes = new HeroRouter();
 //heroRoutes.init();
 
 export default heroRoutes.router;
+
+
+var data = [
+  {
+    "id": 1,
+    "name": "Luke Cage",
+    "aliases": ["Carl Lucas", "Power Man", "Mr. Bulletproof", "Hero for Hire"],
+    "occupation": "bartender",
+    "gender": "male",
+    "height": {
+      "ft": 6,
+      "in": 3
+    },
+    "hair": "bald",
+    "eyes": "brown",
+    "powers": [
+      "strength",
+      "durability",
+      "healing"
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Spider-Man",
+    "aliases": ["Dr. Peter Benjamin Parker", "Spidey", "Web-Sligner", "Spider-X-Man"],
+    "occupation": "scientist",
+    "gender": "male",
+    "height": {
+      "ft": 5,
+      "in": 10
+    },
+    "hair": "brown",
+    "eyes": "hazel",
+    "powers": [
+      "wall-crawling",
+      "strength",
+      "speed",
+      "stamina",
+      "durability",
+      "agility",
+      "healing",
+      "reflexes",
+      "Spider-Sense",
+      "genius"
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Captain America",
+    "aliases": [
+      "Winghead",
+      "Shield-Slinger",
+      "the Captain",
+      "Cap",
+      "Yeoman America",
+      "Sentinel of Liberty",
+      "The Living Legend"
+    ],
+    "occupation": "special agent",
+    "gender": "male",
+    "height": {
+      "ft": 6,
+      "in": 2
+    },
+    "hair": "blonde",
+    "eyes": "blue",
+    "powers": [
+      "strength",
+      "speed",
+      "durability",
+      "agility",
+      "reflexes",
+      "stamina",
+      "healing",
+      "longevity"
+    ]
+  },
+  {
+    "id": 4,
+    "name": "Iron Man",
+    "aliases": [
+      "Tony Stark",
+      "Golden Gladiator",
+      "Spare Parts Man",
+      "Space-Knight"
+    ],
+    "occupation": "inventor",
+    "gender": "male",
+    "height": {
+      "ft": 6,
+      "in": 1
+    },
+    "hair": "black",
+    "eyes": "blue",
+    "powers": []
+  },
+  {
+    "id": 5,
+    "name": "Wolverine",
+    "aliases": [
+      "Logan",
+      "Weapon X",
+      "Death",
+      "Agent Ten",
+      "Fist of Legend"
+    ],
+    "occupation": "special agent",
+    "gender": "male",
+    "height": {
+      "ft": 5,
+      "in": 3
+    },
+    "hair": "black",
+    "eyes": "blue",
+    "powers": [
+      "healing",
+      "acute senses",
+      "strength",
+      "speed",
+      "durability",
+      "agility",
+      "stamina",
+      "weather adaptation",
+      "animal empathy",
+      "bone claws"
+    ]
+  }
+]
